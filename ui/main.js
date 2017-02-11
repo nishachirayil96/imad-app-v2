42b16015e9b337d9cc26836d4e366b5bcb32a1f9
nishachirayil96 <nisha.chirayil96@gmail.com>
2017-02-11 21:20:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,11 @@ console.log('Loaded!');
 alert('Hi!');
 var element=document.getElementById('main-text');
 element.innerHTML='New value';
+
+
+
+var element=document.getElementById('madi');
+
+madi.onclick=function(){
+ing.style.marginLeft='108px';
+};
