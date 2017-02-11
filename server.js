44e1da234043a771676937bf1316efb94f902bc5
nishachirayil96 <nisha.chirayil96@gmail.com>
2017-02-11 20:11:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,94 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articles={
+    articleone :{
+        title:'Article one',
+        heading:'Article one',
+        date:'11th febuary 2017',
+            content:`   <p>Hiee this is a article regarding URL one.This is my first content on web.I m learning this through nptel programme.
+                    this is my first liv e web server. I m enjoying this.
+                    </p>
+                     <p>Hiee this is a article regarding URL.This is my first content on web.I m learning this through nptel programme.
+                    this is my first liv e web server. I m enjoying this.
+                    </p>
+                     <p>Hiee this is a article regarding URL.This is my first content on web.I m learning this through nptel programme.
+                    this is my first liv e web server. I m enjoying this.
+                    </p>
+                    `
+        
+    },
+    articletwo :{
+        title:'Article Two',
+        heading:'Article two',
+        date:'1th febuary 2017',
+            content:`   <p>Hiee this is a article regarding URL two .This is my first content on web.I m learning this through nptel programme.
+                    this is my first liv e web server. I m enjoying this.
+                    </p>
+                     <p>Hiee this is a article regarding URL.This is my first content on web.I m learning this through nptel programme.
+                    this is my first liv e web server. I m enjoying this.
+                    </p>
+                     <p>Hiee this is a article regarding URL.This is my first content on web.I m learning this through nptel programme.
+                    this is my first liv e web server. I m enjoying this.
+                    </p>`
+        
+    },
+    articlethree :{
+        title:'Article three',
+        heading:'Article three',
+        date:'1th febuary 2017',
+            content:`   <p>Hiee this is a article regarding URL three.This is my first content on web.I m learning this through nptel programme.
+                    this is my first liv e web server. I m enjoying this.
+                    </p>
+                     <p>Hiee this is a article regarding URL.This is my first content on web.I m learning this through nptel programme.
+                    this is my first liv e web server. I m enjoying this.
+                    </p>
+                     <p>Hiee this is a article regarding URL.This is my first content on web.I m learning this through nptel programme.
+                    this is my first liv e web server. I m enjoying this.
+                    </p>`
+    },
+
+};
+function createtemplate(data)
+{
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    var htmltemplate =`
+    <html>
+        
+        <head>
+            <title>
+                ${title}
+            </title>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+      
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">home</a>
+                </div>
+                <hr/>
+                <h3>${heading}</h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                   ${content}
+                    
+                    <img src="ui/madi.png" alt="Imad logo" height="42" width="42" >
+                </div>
+            </div>
+                
+        </body>
+    </html>`
+    
+    return htmltemplate;
+}
+
 //send content of ui/index.html 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -12,17 +100,17 @@ app.get('/', function (req, res) {
 
 app.get('/article-one',function(req,res)
 {
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ res.send(createtemplate(articleone));
 });
 
 app.get('/article-two',function(req,res)
 {
- res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+res.send(createtemplate(articletwo));
 });
 
 app.get('/article-three',function(req,res)
 {
-res.send('Article three was requested and will be served here');
+res.send(createtemplate(articlethree));
 });
 
 //if this particular url path is requested this function is excecuted
